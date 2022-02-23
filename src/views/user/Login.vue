e773b86a27fa06ea6b1899876fcd2860c5e653e1
wanqiang <1872757047@qq.com>
2022-02-23 20:21:16 +0800

    add auto import

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,9 +1,10 @@
 <template>
   <h3>Login</h3>
+  <Button type="primary">主要按钮</Button>
 </template>
 
 <script lang="ts" setup>
-
+import {Button} from "vant";
 </script>
 
 <style lang="scss" scoped>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-let UglifyJsPlugin
-UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const tsImportPluginFactory = require('ts-import-plugin')
+const merge = require('webpack-merge')
 
 const isProduction = process.env.NODE_ENV === 'production',
   uglifyJsPlugin = new UglifyJsPlugin({
@@ -30,6 +31,29 @@ module.exports = {
     plugins: isProduction ?
       [uglifyJsPlugin] :
       []
+  },
+  chainWebpack(config) {
+    config.module
+      .rule('ts')
+      .use('ts-loader')
+      .tap(options => {
+        options = merge(options, {
+          transpileOnly: true,
+          getCustomTransformers: () => ({
+            before: [
+              tsImportPluginFactory({
+                libraryName: 'vant',
+                libraryDirectory: 'es',
+                style: true
+              })
+            ]
+          }),
+          compilerOptions: {
+            module: 'es2015'
+          }
+        })
+        return options
+      })
   },
   css: {
     loaderOptions: {
